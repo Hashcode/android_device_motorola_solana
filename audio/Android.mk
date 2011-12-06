70511ffe88e5d06d1efd531ae790607e11a6a8d5
Hashcode <hashcode0f@gmail.com>
2011-12-06 13:32:22 -0800

    added pre-req file copy for the build script (dhacker29)

--- audio/Android.mk
+++ audio/Android.mk
@@ -4,6 +4,38 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
+# output for libaudio intermediates
+LIBAUDIO_INTERMEDIATES_PREREQS := $(PRODUCT_OUT)/obj/lib
+
+# prerequisites for building audio_policy.targa
+file := $(LIBAUDIO_INTERMEDIATES_PREREQS)/libaudio.so
+$(file) : device/motorola/targa/prebuilt/audio/libaudio.so
+	@echo "Copy libaudio.so -> $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) cp -a device/motorola/targa/prebuilt/audio/libaudio.so $@
+
+file := $(LIBAUDIO_INTERMEDIATES_PREREQS)/libaudio_ext.so
+$(file) : device/motorola/targa/prebuilt/audio/libaudio_ext.so
+	@echo "Copy libaudio_ext.so -> $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) cp -a device/motorola/targa/prebuilt/audio/libaudio_ext.so $@
+
+file := $(LIBAUDIO_INTERMEDIATES_PREREQS)/libasound.so
+$(file) : device/motorola/targa/prebuilt/audio/libasound.so
+	@echo "Copy libasound.so -> $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) cp -a device/motorola/targa/prebuilt/audio/libasound.so $@
+
+file := $(LIBAUDIO_INTERMEDIATES_PREREQS)/liba2dp.so
+$(file) : device/motorola/targa/prebuilt/audio/liba2dp.so
+	@echo "Copy liba2dp.so -> $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) cp -a device/motorola/targa/prebuilt/audio/liba2dp.so $@
+
 LOCAL_SRC_FILES:= AudioPolicyManager.cpp
 LOCAL_SHARED_LIBRARIES:= libc libcutils libutils libmedia
 LOCAL_STATIC_LIBRARIES := libmedia_helper
