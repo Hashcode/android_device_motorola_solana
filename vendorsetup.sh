dd09b9dc668b19b0c20ff0e217953dd52b7b60a6
Hashcode <hashcode0f@gmail.com>
2011-12-14 16:13:48 -0800

    changing APN config
    Change-Id: I844d010ec713a15e106a2499afb51dac2a98b48c

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,5 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo generic_solana-userdebug
+add_lunch_combo generic_solana-eng
 add_lunch_combo cm_solana-userdebug
