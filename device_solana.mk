cc8f41eb3543f221ac2ac8f5739adbea9ef85a1c
Hashcode <hashcode0f@gmail.com>
2012-02-13 22:39:37 -0800

    remove GApps
    Change-Id: I5ec59b8d6f4b64e25115641d89568eefb19350d8

--- device_solana.mk
+++ device_solana.mk
@@ -251,7 +251,7 @@ PRODUCT_COPY_FILES += \
 $(call inherit-product, hardware/ti/omap4xxx/omap4.mk)
 $(call inherit-product, hardware/ti/camera/camera.mk)
 $(call inherit-product, hardware/ti/wpan/ti-wpan-products.mk)
-$(call inherit-product-if-exists, vendor/google/google-vendor.mk)
+#$(call inherit-product-if-exists, vendor/google/google-vendor.mk)
 #$(call inherit-product, hardware/ti/wpan/tools/FM/Android.mk)
 
 $(call inherit-product-if-exists, vendor/motorola/solana/solana-vendor.mk)
